6633b4b1978e60278e498b9697f70b4f8a32c9e7
Grant Moore <625518@bah.com>
2022-09-30 15:21:32 -0400

    debugging, use literal list

--- main.tf
+++ main.tf
@@ -2,8 +2,7 @@ resource "aws_kms_key" "automation_library_key" {
     description             = "KMS key for encrypting cluster secrets"
     deletion_window_in_days = 10
     enable_key_rotation     = true
-    custom_master_key_spec  = "SYMMETRIC_DEFAULT"
-    custom_key_store_id     = "automation-library-cluster-key"
+    customer_master_key_spec  = "SYMMETRIC_DEFAULT"
     key_usage               = "ENCRYPT_DECRYPT"
     is_enabled              = true
 }
@@ -26,8 +25,8 @@ resource "aws_security_group_rule" "control_plane_ingress" {
     from_port                 = 0
     to_port                   = 0
     protocol                  = "-1"
-    security_group_id         = aws_security_group.remote_access_sg[0].id
-    source_security_group_id  = aws_security_group.remote_access_sg[0].id
+    security_group_id         = aws_security_group.remote_access_sg.id
+    source_security_group_id  = aws_security_group.remote_access_sg.id
 }
 
 resource "aws_security_group_rule" "control_plane_egress" {
@@ -37,7 +36,7 @@ resource "aws_security_group_rule" "control_plane_egress" {
     to_port                 = 0
     protocol                = "-1"
     cidr_blocks             = ["0.0.0.0/0"]
-    security_group_id         = aws_security_group.remote_access_sg[0].id
+    security_group_id         = aws_security_group.remote_access_sg.id
 }
 
 
@@ -86,7 +85,7 @@ resource "aws_eks_cluster" "automation_library_cluster" {
 
     vpc_config {
         subnet_ids              = var.subnet_ids
-        security_group_ids      = aws_security_group.control_plane_sg.*.id
+        security_group_ids      = [aws_security_group.control_plane_sg.id]
         endpoint_private_access = true
         endpoint_public_access  = false
     }
@@ -114,7 +113,7 @@ resource "aws_eks_node_group" "automation-library-ng1" {
 
     remote_access {
         ec2_ssh_key                 = var.ec2_ssh_key
-        source_security_group_ids   = aws_security_group.remote_access_sg.*.id
+        source_security_group_ids   = [aws_security_group.remote_access_sg.id]
     }
 }
 
