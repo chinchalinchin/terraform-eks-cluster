800e84bb9c54812aebcf48fb4f6555cd9841d56a
Grant Moore <625518@bah.com>
2022-09-30 14:59:35 -0400

    ingest source ips into remote access security group

--- main.tf
+++ main.tf
@@ -30,7 +30,9 @@ resource "aws_security_group" "control_plane_sg" {
     }
 
     tags = {
-        Name = "allow_tls"
+        organization = "AutomationLibrary"
+        team = "BrightLabs"
+        name = "control_plane"
     }
 } 
 resource "aws_security_group" "remote_access_sg" {
@@ -39,23 +41,17 @@ resource "aws_security_group" "remote_access_sg" {
     vpc_id      = var.vpc_id
 
     ingress {
-        description      = "VPC Ingress"
-        from_port        = 0
-        to_port          = 0
-        protocol         = "-1"
-        cidr_blocks      = ["0.0.0.0/0"]
-    }
-
-    egress {
-        description      = "VPC Egress"
-        from_port        = 0
-        to_port          = 0
-        protocol         = "-1"
-        cidr_blocks      = ["0.0.0.0/0"]
+        description      = "VPC SSH Ingress"
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = var.source_ips
     }
 
     tags = {
-        Name = "allow_tls"
+        oganization = "AutomationLibrary"
+        team = "BrightLabs"
+        name = "ssh"
     }
 } 
 
