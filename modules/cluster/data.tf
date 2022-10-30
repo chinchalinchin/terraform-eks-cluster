e7b35d0c9621a9073e4d069488f8583b81d59bb5
Grant Moore <625518@bah.com>
2022-10-30 06:18:48 -0400

    use kube tag to grab vpc in cluster module

--- modules/cluster/data.tf
+++ modules/cluster/data.tf
@@ -2,7 +2,9 @@ data "aws_caller_identity" "current" {}
 
 
 data "aws_vpc" "cluster_vpc" {
-    id                                                  = var.vpc_config.id
+    tags                                                = {
+                                                            "kubernetes.io/cluster/${var.cluster_name}" = "owned" 
+    }   
 }
 
 
